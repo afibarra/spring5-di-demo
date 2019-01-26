aae7869a51183a9a77ff139adeb0666e1d22047e
afibarra <afidroid@gmail.com>
2019-01-25 19:05:54 -0500

    - Reformated Code

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,11 +1,12 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
@@ -14,7 +15,7 @@ public class ConstructorInjectedController {
         this.greetingService = greetingService;
     }
 
-    public String sayHello(){
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 }
